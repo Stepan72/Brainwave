f694601932ab181bf01a342db1f12128dec623b5
Stepan Nimchuk <s.nimchuk@jusan.kz>
2024-08-13 06:37:21 +0500

    finished Header desktop v1.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import ButtonGradient from "@/assets/svg/ButtonGradient";
-import Button from "@/components/Button";
 
 export default function App() {
   return (
-    <div className="w-[400px] h-[400px]">
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] h-[400px] overflow-hidden">
-        <Button className="" href="#love">
-          Something
-        </Button>
+    <div>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem]">
         <ButtonGradient />
       </div>
     </div>
