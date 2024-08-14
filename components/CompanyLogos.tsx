1d779adb6e7460450ef568645c624df4b33112c0
Stepan72 <st.nim@list.ru>
2024-08-14 12:14:20 +0500

    CompanyLogos added v2.0

--- components/CompanyLogos.tsx
+++ components/CompanyLogos.tsx
@@ -1,7 +1,27 @@
+import { companyLogos } from "@/constants";
+import Image from "next/image";
 import React from "react";
 
-const CompanyLogos = () => {
-  return <div>CompanyLogos</div>;
+const CompanyLogos = ({ className }: { className?: string }) => {
+  return (
+    <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        Helping people create beautiful content at
+      </h5>
+      <ul className="flex ">
+        {companyLogos.map((company, index) => {
+          return (
+            <li
+              key={index}
+              className="flex items-center justify-center flex-1 h-[8.5rem]"
+            >
+              <Image src={company} width={134} height={28} alt="company-logo" />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default CompanyLogos;
