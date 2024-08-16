cebdf1085bbfd1cf50ac4afebd340d364d2543e2
Stepan72 <st.nim@list.ru>
2024-08-16 06:46:32 +0500

    Services added v2.3

--- components/Services.tsx
+++ components/Services.tsx
@@ -4,6 +4,13 @@ import Heading from "./Heading";
 import Image from "next/image";
 import { service1, service2, service3, check } from "@/assets";
 import { brainwaveServices, brainwaveServicesIcons } from "@/constants";
+import Generating from "./Generating";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 const Services = () => {
   return (
@@ -15,6 +22,7 @@ const Services = () => {
         />
 
         <div className="relative">
+          {/* First Bento */}
           <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 lx:h-[46rem]">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
               <Image
@@ -25,6 +33,7 @@ const Services = () => {
                 className="w-full h-full object-cover md:object-right"
               />
             </div>
+
             <div className="relative z-1 max-w-[17rem] ml-auto">
               <h4 className="h4 mb-4">Smartest AI</h4>
               <p className="body-2 mb-[3rem] text-n-3">
@@ -49,7 +58,82 @@ const Services = () => {
                 })}
               </ul>
             </div>
+
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg:right-auto lg:bottom-8 lg:-translate-x-1/2" />
           </div>
+          {/* Second Bento */}
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+                <Image
+                  src={service2}
+                  className="w-full h-full object-cover"
+                  alt="robot"
+                  width={630}
+                  height={750}
+                />
+              </div>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                <h4 className="h4 mb-4">Photo Editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Automatically enhance your photos using our AI app's photo
+                  editing feature. Try it now!
+                </p>
+              </div>
+              <PhotoChatMessage />
+            </div>
+
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4">Video Generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  The world's most powerful AI photo and video art generation
+                  engine. What will you create?
+                </p>
+                <ul className="flex justify-between items-center">
+                  {brainwaveServicesIcons.map((icon, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className={`flex rounded-2xl items-center justify-center ${
+                          index === 2
+                            ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem] "
+                            : "w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                        }`}
+                      >
+                        <div
+                          className={
+                            index === 2
+                              ? "flex justify-center items-center w-full h-full bg-n-7 rounded-[1rem]"
+                              : ""
+                          }
+                        >
+                          <Image
+                            src={icon}
+                            alt="service-icon"
+                            width={24}
+                            height={24}
+                          />
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className="relative h-[20rem] md:h-[25rem] bg-n-8 rounded-xl overflow-hidden">
+                <Image
+                  src={service3}
+                  alt="scary-robot"
+                  width={520}
+                  height={400}
+                  className="w-full h-full object-cover"
+                />
+                <VideoChatMessage />
+                <VideoBar />
+              </div>
+            </div>
+          </div>
+          <Gradient />
         </div>
       </div>
     </Section>
